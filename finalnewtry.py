28f49c37a08ec834fe63c0eb9490786083a3ea82
S2 <shashankshowstoper@gmail.com>
2019-02-23 04:44:35 +0530

    Removed delay

--- finalnewtry.py
+++ finalnewtry.py
@@ -2,7 +2,6 @@ import cv2
 import time
 import numpy as np
 cap=cv2.VideoCapture(0)
-#file_path='C:\\Project\\DevSoc19\\'
 while(cap.isOpened()):
     fc=cv2.CascadeClassifier('C:\\Project\\DevSoc19\\haarcascade_frontalface_default.xml')
     ret,frame=cap.read()
@@ -15,16 +14,15 @@ while(cap.isOpened()):
     faces=fc.detectMultiScale(gray)
     #cv2.imshow('Original',frame)
     print("Type => ",type(faces),"\nFaces =>",faces,"\nShape =>",faces.shape)
-#  ERRORRRRRRR!!!!!!!!!!!!!!!!!
+#  ERRORRRRRRR!!!!!!!!!!!!!!!!!  --- Need to resolve L8R
     print("\n\n Number of people => ",str(faces.shape[0]))
-# ERRORRRRRRR!!!!!!!!!!!!!!!!!!
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,100,0),1)
     cv2.rectangle(frame,((0,frame.shape[0]-25)),(270, frame.shape[0]), (255,255,255), -1)
     cv2.putText(frame, "Number of people: " + str(faces.shape[0]), (0,frame.shape[0] -10), cv2.FONT_ITALIC, 0.5, (0,0,0), 1)
     cv2.imshow('Density',frame)
     #cv2.waitkey(3000)
-    time.sleep(1)
+    #time.sleep(1)          Too much laggy --Need 2 remove L8R
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
